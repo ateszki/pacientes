4b76f5bbc83513f500ae7b24a21289f1e3dcf31b
ateszki <ateszki@gmail.com>
2013-08-21 19:58:12 +0000

    key

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'lFXVUuzanAnzWH9Xoq1kHZlYz88zELUV',
 
 	/*
 	|--------------------------------------------------------------------------
