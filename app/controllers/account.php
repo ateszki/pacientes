3749e71cfd33530120e32c401f61c7747222764a
ateszki <ateszki@gmail.com>
2013-09-16 16:42:57 -0300

    prueba controller

---
+++ app/controllers/account.php
@@ -0,0 +1,17 @@
+<?php
+// application/controllers/account.php
+class Account_Controller extends Base_Controller
+{
+	public function action_index()
+	{
+	echo "This is the profile page.";
+	}
+	public function action_login()
+	{
+	echo "This is the login form.";
+	}
+	public function action_logout()
+	{
+	echo "This is the logout action.";
+	}
+}
