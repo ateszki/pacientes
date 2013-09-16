c7dbf8d13bdae1212d903ca23860f6ec59b3b1bf
ateszki <ateszki@gmail.com>
2013-09-16 16:46:56 -0300

    account

--- app/controllers/account.php
+++ app/controllers/account.php
@@ -4,14 +4,14 @@ class Account_Controller extends Base_Controller
 {
 	public function action_index()
 	{
-	echo "This is the profile page.";
+		echo "This is the profile page.";
 	}
 	public function action_login()
 	{
-	echo "This is the login form.";
+		echo "This is the login form.";
 	}
 	public function action_logout()
 	{
-	echo "This is the logout action.";
+		echo "This is the logout action.";
 	}
 }
