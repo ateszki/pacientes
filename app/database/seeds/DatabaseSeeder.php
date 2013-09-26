602d7c705a49e4731cc30391840c0176f68b9f3c
ateszki <ateszki@gmail.com>
2013-09-26 18:46:54 +0000

    migraciones y seeds

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,46 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->call('OdontologoTableSeeder');
 	}
 
+}
+class UserTableSeeder extends Seeder {
+ 
+     public function run()
+     {
+         DB::table('users')->delete();
+         User::create(array(
+                 'id' => 1,
+                 'nombre' => 'admin',
+         	'email' => 'sistemas@consulmed.com.ar',
+                 'password' => Hash::make('admin'),
+                 'created_at' => new DateTime,
+                 'updated_at' => new DateTime
+         ));
+     }
+}
+ 
+class OdontologoTableSeeder extends Seeder {
+    public function run()
+    {
+        DB::table('odontologos')->delete();
+        Odontologo::create(array(
+                'id' => 1,
+                'nombre' => 'Martin',
+		'apellido' => 'Palermo',
+                'matricula' => 'MN 123456',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+        ));
+        Odontologo::create(array(
+                'id' => 1,
+                'nombre' => 'Guillermo',
+		'apellido' => 'Barros Schelotto',
+                'matricula' => 'MN 654321',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+        ));
+    }
 }
---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+        array(
+                'nombre' => 'admin',
+		'email' => 'sistemas@consulmed.com.ar',
+                'password' => Hash::make('admin')
+        ),
+);
