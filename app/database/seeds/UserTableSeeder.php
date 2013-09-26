c760a33c515079c021ceabb2fa33161de58bba77
ateszki <ateszki@gmail.com>
2013-09-26 15:53:21 -0300

    modelo odontologos

--- app/database/seeds/UserTableSeeder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-        array(
-                'nombre' => 'admin',
-		'email' => 'sistemas@consulmed.com.ar',
-                'password' => Hash::make('admin')
-        ),
-);
