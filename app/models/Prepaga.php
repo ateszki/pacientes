7d8372ce84f1bee593b14cbfb71dcd56692b0487
ateszki <ateszki@gmail.com>
2014-08-06 15:20:00 +0000

    arregla fecha prepagas

--- app/models/Prepaga.php
+++ app/models/Prepaga.php
@@ -55,10 +55,12 @@ class Prepaga extends Maestro {
 	public function pacientes(){
 		return $this->belongsToMany('Paciente');
 	}	
+/*
 	public function getFechaAltaAttribute($value){
 		return (!empty($value))?date("d/m/Y",strtotime($value)):'0000-00-00';
 	}
 	public function getFechaBajaAttribute($value){
 		return (!empty($value))?date("d/m/Y",strtotime($value)):'0000-00-00';
 	}
+*/
 }
